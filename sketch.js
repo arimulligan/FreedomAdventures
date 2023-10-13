c07a754779647967991a453c088bcca1ebcb6ca0
Ari <ariannatnz@gmail.com>
2023-10-14 03:59:46 +1300

    made grass and gravity - jumping keeps looping tho

--- sketch.js
+++ sketch.js
@@ -10,52 +10,53 @@ let imagePaths = [
   '/images/Musketeer/Run.png',
   '/images/Musketeer/Walk.png'];
 
-let numFrames = [5, 4, 3, 5, 7, 8, 8];
+let numFrames = [5, 4, 3, 1, 7, 8, 8];
 let animationTypes = ['attack', 'dead', 'hurt', 'idle', 'jump', 'run', 'walk']
-let Musketeer;
+let Musketeer, floor;
 
 function preload() {
-
-}
-
-function setup() {
-  new Canvas(window.innerWidth-4, window.innerHeight-4);
-
-  Musketeer = new Sprite(100, 100, 100);
+  Musketeer = new Sprite(window.innerWidth/2, 0, 500, 128);
 
   // loading all the animations into musketeer
   for (let i = 0; i < NUM_IMAGES; i++){
-    Musketeer.addAni(animationTypes[i], imagePaths[i], { frameSize: [SPRITE_SIZE, SPRITE_SIZE], frames: numFrames[i] });
+    Musketeer.addAni(animationTypes[i], imagePaths[i], { frameSize: [SPRITE_SIZE, SPRITE_SIZE], frames: numFrames[i], frameDelay: 32});
   }
+  Musketeer.changeAni('idle');
   
+}
+
+function setup() {
+  new Canvas(window.innerWidth-4, window.innerHeight-4, 'pixelated x4');
+
+	world.gravity.y = 10;
+	allSprites.pixelPerfect = true;
+
+	floor = new Sprite(0, window.innerHeight/2, window.innerWidth, 32, 's');
+	floor.color = color(133, 228, 62);
 }
 
 function draw() {
   clear();
-	Musketeer.debug = mouse.pressing();
 
-	if (kb.pressing('left')) {
-		Musketeer.changeAni('run');
-		Musketeer.vel.x = -2;
-		Musketeer.mirror.x = true;
-	} else if (kb.pressing('right')) {
-		Musketeer.changeAni('run');
-		Musketeer.vel.x = 2;
-		Musketeer.mirror.x = false;
-	}
-  else if (kb.pressing('down')) {
-    Musketeer.changeAni('idle');
-    Musketeer.vel.y = 2;
+  if (kb.presses('up')) Musketeer.changeAni('jump');
+  if (kb.presses('down')) Musketeer.changeAni('idle');
+  
+  if (kb.pressing('left')) Musketeer.vel.x = -1;
+  else if (kb.pressing('right')) Musketeer.vel.x = 1;
+  else Musketeer.vel.x = 0;
+  
+  if (Musketeer.ani.name != 'jump') {
+    if (kb.pressing('left')) {
+      Musketeer.changeAni('run');
+      Musketeer.mirror.x = true;
+    } else if (kb.pressing('right')) {
+      Musketeer.changeAni('run');
+      Musketeer.mirror.x = false;
+    } else {
+      Musketeer.changeAni('idle');
+    }
   }
-  else if (kb.pressing('up')) {
-    Musketeer.changeAni('jump');
-    Musketeer.vel.y = -2;
-  }
-  else {
-		Musketeer.changeAni('idle');
-		Musketeer.vel.x = 0;
-    Musketeer.vel.y = 0;
-	}
+
 
 	if (kb.pressing('space')) {
 		Musketeer.ani.stop();
